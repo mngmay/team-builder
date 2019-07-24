52c560322390e6796a13c89a417f872d73a605ef
May Ng <mngmay@gmail.com>
2019-07-24 16:09:00 -0700

    Completed step 3 and 4. Added use effect and edit functionality.

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -5,15 +5,36 @@ import "./App.css";
 
 function App() {
   const [members, setMembers] = useState([]);
+  const [memberToEdit, setMemberToEdit] = useState(null);
 
   function addMember(member) {
     setMembers([...members, member]);
+  }
+  console.log(memberToEdit);
+
+  function editMember(member) {
+    const index = members.indexOf(memberToEdit);
+    setMemberToEdit();
+    console.log(index);
+    return setMembers(
+      members.map((person, i) => {
+        if (i === index) {
+          return (person = member);
+        } else {
+          return person;
+        }
+      })
+    );
   }
 
   return (
     <div className="App">
       Here's where the form should be
-      <Form addMember={addMember} />
+      <Form
+        addMember={addMember}
+        memberToEdit={memberToEdit}
+        editMember={editMember}
+      />
       <div>
         {members.map(person => {
           return (
@@ -21,7 +42,8 @@ function App() {
               <li>
                 Name: {person.name} Email: {person.email} Role: {person.role}
               </li>
-              <button>Edit</button>
+              <button onClick={() => setMemberToEdit(person)}>Edit</button>
+              {console.log("memberToEdit", memberToEdit)}
             </div>
           );
         })}
--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Form = props => {
-  const { addMember } = props;
+  const { addMember, memberToEdit, editMember } = props;
   const [member, setMember] = useState({
     name: "Name",
     email: "Email",
@@ -22,41 +22,98 @@ const Form = props => {
   function handleSubmit(event) {
     const add = addMember;
     add(member);
+    setMember({
+      name: "Name",
+      email: "Email",
+      role: "Role"
+    });
     event.preventDefault();
     console.log("member", member);
   }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <fieldset>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          name="name"
-          placeholder="Enter your name"
-          value={member.name}
-          onChange={handleChange}
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          placeholder="Enter email"
-          value={member.email}
-          onChange={handleChange}
-        />
-        <label htmlFor="role">Role</label>
-        <input
-          type="text"
-          name="role"
-          placeholder="Enter your role"
-          value={member.role}
-          onChange={handleChange}
-        />
-        <button type="submit">Submit</button>
-      </fieldset>
-    </form>
-  );
+  useEffect(() => {
+    if (memberToEdit) {
+      setMember(memberToEdit);
+    }
+  }, [memberToEdit]);
+
+  function editPerson(event) {
+    const edit = editMember;
+    edit(member);
+    setMember({
+      name: "Name",
+      email: "Email",
+      role: "Role"
+    });
+    console.log("edited");
+    event.preventDefault();
+  }
+
+  if (memberToEdit) {
+    return (
+      <form onSubmit={editPerson}>
+        <fieldset>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            value={member.name}
+            onChange={handleChange}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            value={member.email}
+            onChange={handleChange}
+          />
+          <label htmlFor="role">Role</label>
+          <input
+            type="text"
+            name="role"
+            placeholder="Enter your role"
+            value={member.role}
+            onChange={handleChange}
+          />
+          <button type="submit">Submit</button>
+        </fieldset>
+      </form>
+    );
+  } else {
+    return (
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            value={member.name}
+            onChange={handleChange}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            value={member.email}
+            onChange={handleChange}
+          />
+          <label htmlFor="role">Role</label>
+          <input
+            type="text"
+            name="role"
+            placeholder="Enter your role"
+            value={member.role}
+            onChange={handleChange}
+          />
+          <button type="submit">Submit</button>
+        </fieldset>
+      </form>
+    );
+  }
 };
 
 export default Form;
